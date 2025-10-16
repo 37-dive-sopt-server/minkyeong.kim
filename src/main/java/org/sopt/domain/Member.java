bbc4c26edaaeb8012791114356c060da3813e58f
kyoooooong <haileyre000b@naver.com>
2025-10-16 20:01:22 +0900

    feat: gender enum 생성, member class 생성

--- src/main/java/org/sopt/domain/Member.java
+++ src/main/java/org/sopt/domain/Member.java
@@ -1,20 +1,31 @@
 package org.sopt.domain;
 
+import java.time.LocalDate;
+import org.sopt.enums.Gender;
+
 public class Member {
 
     private Long id;
     private String name;
+    private LocalDate birthDate;
+    private String email;
+    private Gender gender;
 
-    public Member(Long id, String name) {
+    private Member(Long id, String name, LocalDate birthDate, String email, Gender gender) {
         this.id = id;
         this.name = name;
+        this.birthDate = birthDate;
+        this.email = email;
+        this.gender = gender;
     }
 
-    public Long getId() {
-        return id;
+    public static Member create(Long id, String name, LocalDate birthDate, String email, Gender gender) {
+        return new Member(id, name, birthDate, email, gender);
     }
 
-    public String getName() {
-        return name;
-    }
+    public Long getId() { return id;}
+    public String getName() { return name; }
+    public LocalDate getBirthDate() { return birthDate; }
+    public String getEmail() { return email; }
+    public Gender getGender() { return gender; }
 }
